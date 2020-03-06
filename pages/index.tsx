d42adf0adee8bce4a3413b8f3ad224802da1466f
unknown <jeanchristophedemar@yahoo.fr>
2020-03-06 13:57:57 +0100

    transform in tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,4 +1,18 @@
-import Head from 'next/head'
+import Head from 'next/head';
+import { FormEvent } from 'react';
+import { NextPage } from 'next';
+
+
+const submission = (e:FormEvent<HTMLFormElement>)=>{
+e.preventDefault();
+
+    console.log("test");
+
+}
+
+
+
+
 
 const Home = () => (
   <div className="container">
@@ -8,54 +22,29 @@ const Home = () => (
     </Head>
 
     <main>
-      <h1 className="title">
-        Welcome to <a href="https://nextjs.org">Next.js!</a>
-      </h1>
-
-      <p className="description">
-        Get started by editing <code>pages/index.js</code>
-      </p>
-
-      <div className="grid">
-        <a href="https://nextjs.org/docs" className="card">
-          <h3>Documentation &rarr;</h3>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
-
-        <a href="https://nextjs.org/learn" className="card">
-          <h3>Learn &rarr;</h3>
-          <p>Learn about Next.js in an interactive course with quizzes!</p>
-        </a>
-
-        <a
-          href="https://github.com/zeit/next.js/tree/master/examples"
-          className="card"
-        >
-          <h3>Examples &rarr;</h3>
-          <p>Discover and deploy boilerplate example Next.js projects.</p>
-        </a>
-
-        <a
-          href="https://zeit.co/new?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          className="card"
-        >
-          <h3>Deploy &rarr;</h3>
-          <p>
-            Instantly deploy your Next.js site to a public URL with ZEIT Now.
-          </p>
-        </a>
-      </div>
+      <form name="contact" method="POST" data-netlify="true" onSubmit ={submission}>
+        <p>
+          <label>English <input type="text" name="french" /></label>
+        </p>
+        <p>
+          <label>French <input type="text" name="english" /></label>
+        </p>
+        <p>
+          <button type="submit">Send</button>
+        </p>
+      </form>
     </main>
 
     <footer>
       <a
-        href="https://zeit.co?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
+        href=""
         target="_blank"
         rel="noopener noreferrer"
       >
         Powered by <img src="/zeit.svg" alt="ZEIT Logo" />
       </a>
     </footer>
+
 
     <style jsx>{`
       .container {
@@ -199,5 +188,8 @@ const Home = () => (
     `}</style>
   </div>
 )
+
+
+
 
 export default Home
